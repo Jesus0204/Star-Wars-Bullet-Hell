d67252a1635f73a52ba6216d72e3e28853040eff
Jesus0204 <a01705442@tec.mx>
2024-11-15 09:47:26 -0600

    feat: agregar vida a los Tie Fighters para destruirlos cuando se les acabe la vida

--- Bullet Hell Game/Assets/Scripts/TieFighter.cs
+++ Bullet Hell Game/Assets/Scripts/TieFighter.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class TieFighter : MonoBehaviour
 {
     public string objectToDestroy;
+    public string laserTag;
+
+    public int lifeLeft;
 
     void OnBecameInvisible()
     {
@@ -15,6 +18,19 @@ public class TieFighter : MonoBehaviour
         {
             // Destruye el TieFighter
             Destroy(gameObject);
+        }
+
+        if (collision.gameObject.CompareTag(laserTag))
+        {
+            if (lifeLeft - 1 > 0)
+            {
+                lifeLeft--;
+            }
+            else
+            {
+                // Destruye el TieFighter
+                Destroy(gameObject);
+            }
         }
 
         // Opcional: Destruye el objeto con el que colisiona
